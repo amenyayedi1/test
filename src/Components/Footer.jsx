89fbd3f08d9a2abd7bd459587ca451662f63ae53
amenyayedi <amenyayedi@amenyayedis-MacBook-Air.local>
2023-11-09 16:57:01 +0100

    c

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <div><h1 style={{color:"Gold"}} >this is footer</h1></div>
+    )
+  }
+}
